475f524e0dbe4c7824ab13979fe65348a80a33de
AntaresFS <vbravo.quintana@gmail.com>
2025-03-23 16:06:34 +0100

    Primera migración realizada

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,23 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = Flask
+db = SQLAlchemy()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # Importa tus modelos para que Flask-Migrate los detecte
+    from api.models import User, Food, Meal, Diet, Routine, ProgressRecord, Notification, Message 
+
+    return app
+
+app = create_app()
+
+if __name__ == '__main__':
+    app.run(debug=True)
