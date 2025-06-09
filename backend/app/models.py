1f31916f99e33915866a44eddbc0f27d2b708271
AntaresFS <vbravo.quintana@gmail.com>
2025-06-09 22:45:53 +0200

    Reseteado el monorepo

--- backend/app/models.py
+++
@@ -1,8 +0,0 @@
-from . import db
-
-class Item(db.Model):
-    id   = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(128), nullable=False)
-
-    def to_dict(self):
-        return {'id': self.id, 'name': self.name}
